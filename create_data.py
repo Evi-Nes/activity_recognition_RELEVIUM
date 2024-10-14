9b9873885f45a79d8c44b6b62450293770b5ead9
Evi-Nes <evi.nestoro@gmail.com>
2024-10-14 19:17:54 +0300

    send the actual data to server

--- create_data.py
+++ create_data.py
@@ -21,32 +21,45 @@ client = InfluxDBClient(url="http://localhost:8085", token="ax1hjMD3MVseMkM4Zg1t
 write_api = client.write_api(write_options=SYNCHRONOUS)
 
 try:
-    while True:
-        patient_id = "RELEVIUM-MAINZ-01-01"
-        device = "Smartwatch"
-        date = "20241014"
-        # timewindow = 1
-        tz = "03:00"
-        sensor_value_x = 1.6855
-        sensor_value_y = 4.6731
-        sensor_value_z = 5.0779
-        dt = datetime.strptime("2024-10-14 17:30:00", "%Y-%m-%d %H:%M:%S")
-        timestamp = int(dt.timestamp() * 1000)   # to milliseconds
+    with open('a_s_RELEVIUM-MAINZ-01-01_20200202_001', 'r') as file:
 
-        # Create a data point in the required line protocol format
-        line_protocol = (
-            f"accelerometer,patient_id={patient_id},device={device},"
-            f"date={date},tz={tz} "
-            f"x={sensor_value_x},y={sensor_value_y},z={sensor_value_z} "
-            f"{timestamp}"
-        )
-        # timewindow={timewindow},
-        # Write the data point to InfluxDB
-        write_api.write(bucket=bucket, org=org, record=line_protocol, precision='ms')
+        for line in file:
+            if line:
+                # Split the line into the measurement, the fields and the timestamp
+                measurement_part, fields_part, timestamp_part = line.split(' ', 2)
 
-        print(f"Uploaded data: {line_protocol}")
+                accelerometer, tags_part = measurement_part.split(',', 1)
+                tags = tags_part.split(',')
+                tags_dict = {tag.split('=')[0]: tag.split('=')[1] for tag in tags}
 
-        time.sleep(1)
+                # Split fields part by commas to get field values
+                fields = fields_part.split(',')
+                fields_dict = {field.split('=')[0]: float(field.split('=')[1]) for field in fields}
+
+            patient_id = tags_dict['patient_id']
+            device = tags_dict['device']
+            date = "20241014"
+            tz = tags_dict['tz']
+            sensor_value_x = fields_dict['x']
+            sensor_value_y = fields_dict['y']
+            sensor_value_z = fields_dict['z']
+            dt = datetime.strptime("2024-10-14 17:30:00", "%Y-%m-%d %H:%M:%S")
+            timestamp = int(dt.timestamp() * 1000)   # to milliseconds
+
+            # Create a data point in the required line protocol format
+            line_protocol = (
+                f"accelerometer,patient_id={patient_id},device={device},"
+                f"date={date},tz={tz} "
+                f"x={sensor_value_x},y={sensor_value_y},z={sensor_value_z} "
+                f"{timestamp}"
+            )
+
+            # Write the data point to InfluxDB
+            write_api.write(bucket=bucket, org=org, record=line_protocol, precision='ms')
+
+            print(f"Uploaded data: {line_protocol}")
+
+            time.sleep(1)
 
 except KeyboardInterrupt:
     print("Live data upload stopped.")
