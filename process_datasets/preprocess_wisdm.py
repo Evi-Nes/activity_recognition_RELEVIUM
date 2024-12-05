9314639bf91d35412f8554263d274210acbbbd36
Evi Nestoropoulou <evi.nestoro@gmail.com>
2024-12-05 22:33:37 +0200

    update final wisdm

--- process_datasets/preprocess_wisdm.py
+++ process_datasets/preprocess_wisdm.py
@@ -1,6 +1,7 @@
 import os
 from datetime import datetime, timedelta
 
+import numpy as np
 import pandas as pd
 
 accel_folder = ('wisdm+smartphone+and+smartwatch+activity+and+biometrics+dataset/wisdm-dataset/raw/watch/accel')
@@ -17,58 +18,34 @@ for file in os.listdir(accel_folder):
         accel_data.columns = ['user_id', 'activity', 'timestamp', 'accel_x', 'accel_y', 'accel_z']
         accel_data['accel_z'] = accel_data['accel_z'].str.rstrip(';')
 
-        accel_data['timestamp'] = accel_data['timestamp'].astype(int)
         user_id = accel_data['user_id'][0]
 
         # 20Hz -> 25Hz
-        # print(accel_data.index.duplicated().sum())
-        # print('\n')
-
-        # Set timestamp as index
-        if accel_data['timestamp'][0] > 14:
-            unit = 'us'
-        else:
-            unit = 'ms'
-        accel_data = accel_data.sort_values(by='timestamp')
-        accel_data['datetime'] = pd.to_datetime(accel_data['timestamp'], unit=unit)
-        print(f"Timestamp Range: {accel_data['timestamp'].min()} to {accel_data['timestamp'].max()}")
-
-        accel_data['interval'] = accel_data['datetime'].diff().dt.total_seconds()
-        frequency = accel_data['interval'].mean()
-        print(f"Approximate frequency: {frequency} seconds")
-
+        n_rows = len(accel_data)
         yesterday = datetime.now() - timedelta(days=1)
-        accel_data['yesterday_datetime'] = accel_data['datetime'].apply(lambda x: x.replace(
-            year=yesterday.year, month=yesterday.month, day=yesterday.day))
+        frequency = timedelta(milliseconds=50)
+        timestamps = [yesterday + i * frequency for i in range(n_rows)]
 
-        # Print results
-        print(accel_data[['timestamp', 'datetime', 'yesterday_datetime']])
+        accel_data['timestamp'] = timestamps
 
+        accel_data = accel_data[['timestamp', 'user_id', 'activity', 'accel_x', 'accel_y', 'accel_z']]
+        accel_data.set_index('timestamp', inplace=True)
+        accel_data.dropna()
+        # print(accel_data.head)
 
-        accel_data = accel_data.sort_values(by='datetime')
+        numeric_cols = ['accel_x', 'accel_y', 'accel_z']
+        categorical_cols = ['user_id', 'activity']
+        numeric_resampled = accel_data[numeric_cols].resample('40ms').apply(lambda x: np.nan if x.empty else x.interpolate(method='linear', limit_direction='both'))
+        categorical_resampled = accel_data[categorical_cols].resample('40ms').ffill()
 
-        accel_data['interval'] = accel_data['datetime'].diff().dt.total_seconds()
-        print(accel_data['interval'].describe())
-
-        accel_data.set_index('datetime', inplace=True)
-        # print(accel_data.head())
-        # print(df['timestamp'].diff().describe())
-        accel_data = accel_data.resample('50ms').interpolate()
-        accel_data.reset_index(inplace=True)
-
-        accel_data['interval'] = accel_data['datetime'].diff().dt.total_seconds() * 1000
-        print(accel_data['interval'].describe())
-
-        print(accel_data.index.duplicated().sum())
-        print('\n')
-
-        # df.asfreq(freq='40ms', method='ffill')
-        # df = df.reset_index()
-        accel_data = accel_data.resample('40ms').ffill()
-        accel_data = accel_data.reset_index()
+        accel_data_resampled = pd.concat([numeric_resampled, categorical_resampled], axis=1).reset_index()
+        accel_data_resampled = accel_data_resampled.fillna(method='ffill').fillna(method='bfill')
+        accel_data_resampled = accel_data_resampled[['timestamp', 'user_id', 'activity', 'accel_x', 'accel_y', 'accel_z']]
+        print(accel_data_resampled.head)
+        # print(accel_data_resampled.isna().sum())
 
         merged_file_path = os.path.join(accel_folder, f'merged_user_{user_id}.csv')
-        accel_data.to_csv(merged_file_path, index=False)
+        accel_data_resampled.to_csv(merged_file_path, index=False)
 
         merged_files.append(merged_file_path)
 
@@ -97,8 +74,32 @@ for file in os.listdir(gyro_folder):
         gyro_data['user_id'] = gyro_data['user_id']
         user_id = gyro_data['user_id'][0]
 
+        # 20Hz -> 25Hz
+        n_rows = len(gyro_data)
+        yesterday = datetime.now() - timedelta(days=1)
+        frequency = timedelta(milliseconds=50)
+        timestamps = [yesterday + i * frequency for i in range(n_rows)]
+
+        gyro_data['timestamp'] = timestamps
+
+        gyro_data = gyro_data[['timestamp', 'user_id', 'activity', 'gyro_x', 'gyro_y', 'gyro_z']]
+        gyro_data.set_index('timestamp', inplace=True)
+        gyro_data.dropna()
+        # print(gyro_data.head)
+
+        numeric_cols = ['gyro_x', 'gyro_y', 'gyro_z']
+        categorical_cols = ['user_id', 'activity']
+        numeric_resampled = gyro_data[numeric_cols].resample('40ms').apply(lambda x: np.nan if x.empty else x.interpolate(method='linear', limit_direction='both'))
+        categorical_resampled = gyro_data[categorical_cols].resample('40ms').ffill()
+
+        gyro_data_resampled = pd.concat([numeric_resampled, categorical_resampled], axis=1).reset_index()
+        gyro_data_resampled = gyro_data_resampled.fillna(method='ffill').fillna(method='bfill')
+        gyro_data_resampled = gyro_data_resampled[['timestamp', 'user_id', 'activity','gyro_x', 'gyro_y', 'gyro_z']]
+        print(gyro_data_resampled.head)
+        # print(gyro_data_resampled.isna().sum())
+
         merged_file_path = os.path.join(gyro_folder, f'merged_user_{user_id}.csv')
-        gyro_data.to_csv(merged_file_path, index=False)
+        gyro_data_resampled.to_csv(merged_file_path, index=False)
 
         merged_files.append(merged_file_path)
 
