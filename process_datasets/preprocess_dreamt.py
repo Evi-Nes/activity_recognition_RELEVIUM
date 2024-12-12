3d93f8b6681bc10ffe4e9f43138e213cead0c965
Evi Nestoropoulou <evi.nestoro@gmail.com>
2024-12-12 12:43:31 +0200

    update dreamt with correct frequency

--- process_datasets/preprocess_dreamt.py
+++ process_datasets/preprocess_dreamt.py
@@ -9,35 +9,55 @@ merged_files = []
 
 for filename in os.listdir(main_folder):
     user_id = filename.replace('S0', '').replace('_whole_df.csv', '')
-
     df = pd.read_csv(os.path.join(main_folder, filename))
     df = df[df['Sleep_Stage'].isin(desired_stages)]
-
     df.rename(columns={'TIMESTAMP': 'timestamp', 'ACC_X': 'accel_x', 'ACC_Y': 'accel_y', 'ACC_Z': 'accel_z', 'Sleep_Stage': 'activity'}, inplace=True)
-
     mapping = {'W': 'lying', 'N1': 'sleeping', 'N2': 'sleeping', 'N3': 'sleeping', 'R': 'sleeping'}
+
     df['activity'] = df['activity'].replace(mapping)
     df['user_id'] = user_id + 'DR'
-
+    df['timestamp'] = pd.to_datetime(df['timestamp'], unit='s')
     df = df[['timestamp', 'user_id', 'activity', 'accel_x', 'accel_y', 'accel_z']]
-    unique_activities = df['activity'].unique()
-    for activity in unique_activities:
-        print(activity)
-        print(count(df[df['activity'] == activity]))
 
+    numeric_cols = ['accel_x', 'accel_y', 'accel_z']
+    categorical_cols = ['user_id', 'activity']
+    # print(df.index.to_series().diff().value_counts())
+
+    # print(df)
+    print(df['activity'].value_counts())
+
+    df = df.iloc[::2].reset_index(drop=True)
     df = df.set_index('timestamp')
-    new_timestamps = np.arange(df.index.min(), df.index.max(), 1 / 25)
 
-    resampled_data = pd.DataFrame(index=new_timestamps)
-    resampled_data = resampled_data.join(df)
+    # print(df)
+    print(df['activity'].value_counts())
 
-    resampled_data[['accel_x', 'accel_y', 'accel_z']] = resampled_data[['accel_x', 'accel_y', 'accel_z']].interpolate()
-    resampled_data[['user_id', 'activity']] = resampled_data[['user_id', 'activity']].fillna(method='ffill')
-    unique_activities = resampled_data['activity'].unique()
-    for activity in unique_activities:
-        print(activity)
-        print(count(resampled_data[resampled_data['activity'] == activity]))
+    # Resample numeric columns and interpolate
+    numeric_resampled = df[numeric_cols].resample('40ms').apply(
+        lambda x: x.interpolate(method='linear', limit_direction='both') if not x.empty else np.nan)
+    categorical_resampled = df[categorical_cols].resample('40ms').ffill()
 
+    data_resampled = pd.concat([numeric_resampled, categorical_resampled], axis=1)
+    data_resampled = data_resampled.fillna(method='ffill').fillna(method='bfill')
+
+    print(data_resampled)
+
+    original_timestamps = df.index
+    # rounded_original_timestamps = df.index.round('ms')
+    # rounded_resampled_timestamps = data_resampled.index.round('ms')
+
+    mask = ~data_resampled.index.isin(original_timestamps)
+    data_resampled_filtered = data_resampled[mask]
+
+    print(data_resampled_filtered)
+
+    data_resampled_filtered.reset_index(inplace=True)
+    # data_resampled.rename(columns={'index': 'timestamp'}, inplace=True)
+    data_resampled_filtered.dropna(inplace=True)
+    print(data_resampled_filtered)
+    print(data_resampled_filtered['activity'].value_counts())
+
+    prit
     resampled_data.reset_index(inplace=True)
     resampled_data.rename(columns={'index': 'timestamp'}, inplace=True)
 
