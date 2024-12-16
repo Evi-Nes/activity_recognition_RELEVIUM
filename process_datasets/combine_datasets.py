a30dd09aa5fea9dfaf607771907e341f99162836
Evi Nestoropoulou <evi.nestoro@gmail.com>
2024-12-16 10:48:05 +0200

    add final combined dataset7

--- process_datasets/combine_datasets.py
+++ process_datasets/combine_datasets.py
@@ -1,24 +1,16 @@
 import pandas as pd
-from numpy.ma.core import count
 
 df_domino = pd.read_csv('final_domino.csv')
 df_pamap2 = pd.read_csv('final_pamap2.csv')
 df_wisdm = pd.read_csv('final_wisdm.csv')
-df_walk_running = pd.read_csv('final_walk_running.csv')
-df_static_exercising = pd.read_csv('final_static_exercise.csv')
-df_dynamic_exercising = pd.read_csv('final_dynamic_exercise.csv')
+df_walk_running = pd.read_csv('final_walking_running.csv')
+df_static_exercising = pd.read_csv('final_static_exercising.csv')
+df_dynamic_exercising = pd.read_csv('final_dynamic_exercising.csv')
 df_dreamt = pd.read_csv('final_dreamt.csv')
-# df_multilevel_1_2 = pd.read_csv('final_multilevel_1-2.csv')
-# df_multilevel_sleep = pd.read_csv('final_multilevel_sleep.csv')
 
 combined_df = pd.concat([df_domino, df_pamap2, df_wisdm, df_walk_running, df_static_exercising, df_dynamic_exercising, df_dreamt], axis=0, ignore_index=True)
 print(combined_df.head())
 
-# unique_activities = combined_df['activity'].unique()
-#
-# for activity in unique_activities:
-#     print(activity)
-#     print(count(combined_df[combined_df['activity'] == activity]))
 print(combined_df['activity'].value_counts())
 
 combined_df.to_csv('combined_dataset7.csv', index=False)
