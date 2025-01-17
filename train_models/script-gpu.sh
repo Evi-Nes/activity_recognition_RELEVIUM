a793902ef150e5418333d7dddc00b9afc379a4ef
Evi-Nes <evi.nestoro@gmail.com>
2025-01-17 16:36:14 +0200

    update script

--- train_models/script-gpu.sh
+++ train_models/script-gpu.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH -c4 --mem 12G --gres shard:20 -t 0-50
+#SBATCH -c4 --mem 12G --gres shard:16 -t 0-50
 
 cd /mnt/cephfs/home/nestoropo/activity_recognition_RELEVIUM/train_models
 
@@ -8,4 +8,4 @@ source ~/venv_gpu/bin/activate
 
 
 echo "Training Models with gpu"
-python accelerometer_models.py
+python accelerometer_models_9_classes.py
