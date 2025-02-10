8f25551d374030fd77b77942ccee262bcf84587e
Evi Nestoropoulou <evi.nestoro@gmail.com>
2025-02-10 17:19:16 +0200

    update code

--- process_datasets/preprocess_dreamt.py
+++ process_datasets/preprocess_dreamt.py
@@ -39,52 +39,52 @@ def process_acceleration(acc_data, sampling_rate=32, cutoff_freq=0.3):
 
     return linear_acc
 
-for filename in os.listdir(main_folder):
-    if not filename.endswith('_whole_df.csv'):
-        continue
-    print(filename)
-    user_id = filename.replace('S0', '').replace('_whole_df.csv', '')
-    df = pd.read_csv(os.path.join(main_folder, filename))
-    df = df[df['Sleep_Stage'].isin(desired_stages)]
-    df.rename(columns={'TIMESTAMP': 'timestamp', 'ACC_X': 'accel_x', 'ACC_Y': 'accel_y', 'ACC_Z': 'accel_z', 'HR': 'hr', 'Sleep_Stage': 'activity'}, inplace=True)
-
-    df['user_id'] = user_id + 'DR'
-    df['timestamp'] = pd.to_datetime(df['timestamp'], unit='s')
-    df = df[['timestamp', 'user_id', 'activity', 'accel_x', 'accel_y', 'accel_z', 'hr']]
-
-    # Timestamp at 64Hz, accelerometer at 32Hz
-    df = df.iloc[::2].reset_index(drop=True)
-    df = df.set_index('timestamp')
-    # print('Initial data', df['activity'].value_counts())
-    # print(df.head())
-
-    # Resample numeric columns and interpolate
-    numeric_resampled = df[numeric_cols].resample('40ms').apply(
-        lambda x: x.interpolate(method='linear', limit_direction='both') if not x.empty else np.nan)
-    categorical_resampled = df[categorical_cols].resample('40ms').ffill()
-
-    data_resampled = pd.concat([numeric_resampled, categorical_resampled], axis=1)
-    data_resampled = data_resampled.ffill()
-
-    original_timestamps = df.index
-    mask = ~data_resampled.index.isin(original_timestamps)
-    data_resampled_filtered = data_resampled[mask]
-
-    data_resampled_filtered.reset_index(inplace=True)
-    data_resampled_filtered.dropna(inplace=True)
-    data_resampled_filtered = data_resampled_filtered[['timestamp', 'user_id', 'activity', 'accel_x', 'accel_y', 'accel_z', 'hr']]
-
-    lying_data = data_resampled_filtered[data_resampled_filtered['activity'] == 'W']
-    sleeping_data = data_resampled_filtered[data_resampled_filtered['activity'].isin(['N1', 'N2', 'N3', 'R'])]
-
-    retain_fraction = 0.7  #keep those
-    downsampled_sleeping = (sleeping_data.groupby('activity', group_keys=False)
-                            .apply(lambda x: x.sample(frac=retain_fraction, random_state=42)))
-    reduced_data = pd.concat([lying_data, downsampled_sleeping])
-    # print('Reduced data', reduced_data['activity'].value_counts())
-
-    reduced_data.to_csv(os.path.join(main_folder, filename.replace('_whole_df.csv', '_processed_25Hz.csv')))
-    print(f'Merged {filename}')
+# for filename in os.listdir(main_folder):
+#     if not filename.endswith('_whole_df.csv'):
+#         continue
+#     print(filename)
+#     user_id = filename.replace('S0', '').replace('_whole_df.csv', '')
+#     df = pd.read_csv(os.path.join(main_folder, filename))
+#     df = df[df['Sleep_Stage'].isin(desired_stages)]
+#     df.rename(columns={'TIMESTAMP': 'timestamp', 'ACC_X': 'accel_x', 'ACC_Y': 'accel_y', 'ACC_Z': 'accel_z', 'HR': 'hr', 'Sleep_Stage': 'activity'}, inplace=True)
+#
+#     df['user_id'] = user_id + 'DR'
+#     df['timestamp'] = pd.to_datetime(df['timestamp'], unit='s')
+#     df = df[['timestamp', 'user_id', 'activity', 'accel_x', 'accel_y', 'accel_z', 'hr']]
+#
+#     # Timestamp at 64Hz, accelerometer at 32Hz
+#     df = df.iloc[::2].reset_index(drop=True)
+#     df = df.set_index('timestamp')
+#     # print('Initial data', df['activity'].value_counts())
+#     # print(df.head())
+#
+#     # Resample numeric columns and interpolate
+#     numeric_resampled = df[numeric_cols].resample('40ms').apply(
+#         lambda x: x.interpolate(method='linear', limit_direction='both') if not x.empty else np.nan)
+#     categorical_resampled = df[categorical_cols].resample('40ms').ffill()
+#
+#     data_resampled = pd.concat([numeric_resampled, categorical_resampled], axis=1)
+#     data_resampled = data_resampled.ffill()
+#
+#     original_timestamps = df.index
+#     mask = ~data_resampled.index.isin(original_timestamps)
+#     data_resampled_filtered = data_resampled[mask]
+#
+#     data_resampled_filtered.reset_index(inplace=True)
+#     data_resampled_filtered.dropna(inplace=True)
+#     data_resampled_filtered = data_resampled_filtered[['timestamp', 'user_id', 'activity', 'accel_x', 'accel_y', 'accel_z', 'hr']]
+#
+#     lying_data = data_resampled_filtered[data_resampled_filtered['activity'] == 'W']
+#     sleeping_data = data_resampled_filtered[data_resampled_filtered['activity'].isin(['N1', 'N2', 'N3', 'R'])]
+#
+#     retain_fraction = 0.7  #keep those
+#     downsampled_sleeping = (sleeping_data.groupby('activity', group_keys=False)
+#                             .apply(lambda x: x.sample(frac=retain_fraction, random_state=42)))
+#     reduced_data = pd.concat([lying_data, downsampled_sleeping])
+#     # print('Reduced data', reduced_data['activity'].value_counts())
+#
+#     reduced_data.to_csv(os.path.join(main_folder, filename.replace('_whole_df.csv', '_processed_25Hz.csv')))
+#     print(f'Merged {filename}')
 
 # Combine 25Hz data with classes N1, N2, N3, R, W
 all_data = []
