c2adac219673e5bd76052157454d61d8a67ff11a
Evi Nestoropoulou <evi.nestoro@gmail.com>
2025-02-10 17:52:53 +0200

    update code and have final 9 classes data

--- process_datasets/combine_classes.py
+++ process_datasets/combine_classes.py
@@ -1,12 +1,12 @@
 import pandas as pd
 
 data = pd.read_csv('combined_dreamt_25Hz.csv')
+
+data = data[:int(len(data)*0.6)]
+print(data['activity'].value_counts())
+
 mapping = {'W': 'lying', 'N1': 'sleeping', 'N2': 'sleeping', 'N3': 'sleeping', 'R': 'sleeping'}
 data['activity'] = data['activity'].replace(mapping)
-unique_activities = data['activity'].unique()
-for activity in unique_activities:
-    data = data.loc[data['activity'] == activity, :int(len(data)*0.7)]
 
-print(data['activity'].value_counts())
 data.to_csv('final_dreamt_25Hz.csv', index=False)
 
