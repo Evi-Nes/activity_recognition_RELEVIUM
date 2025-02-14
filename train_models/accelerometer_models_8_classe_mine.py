d7a33ab8d2985bd2c2f0526a41fdb911ef5418b5
Evi-Nes <evi.nestoro@gmail.com>
2025-02-14 16:55:02 +0200

    update file for my data

--- train_models/accelerometer_models_8_classe_mine.py
+++ train_models/accelerometer_models_8_classe_mine.py
@@ -110,6 +110,12 @@ def process_data(path, timesteps, testing):
     data = data[['activity', 'accel_x', 'accel_y', 'accel_z']]
     data = data.dropna()
     data['activity'] = data['activity'].replace('static_exercising', 'dynamic_exercising')
+    if not testing:
+        data = data[data['activity'] != 'dynamic_exercising']
+        data = data[data['activity'] != 'sleeping']
+        data = data[data['activity'] != 'lying']
+        data = data[data['activity'] != 'cycling']
+
     unique_activities = data['activity'].unique()
 
     x_data, y_data = create_sequences(data[['accel_x', 'accel_y', 'accel_z']], data['activity'], timesteps, unique_activities)
@@ -200,7 +206,7 @@ def create_sequential_model(X_train, y_train, chosen_model, input_shape, file_na
                   metrics=[keras.metrics.CategoricalAccuracy()])
 
     model.fit(X_train, y_train, epochs=30, batch_size=64, validation_split=0.2, verbose=2)
-    model.save(f'{file_name}.keras')
+    model.save(f'{file_name}')
 
     return model
 
@@ -215,7 +221,7 @@ def train_sequential_model(X_train, y_train, X_test, y_test, chosen_model, class
     if not os.path.exists(f'files_{filename}/saved_models_{filename}'):
         os.makedirs(f'files_{filename}/saved_models_{filename}')
 
-    file_name = f'files_{filename}/saved_models_{filename}/acc_{chosen_model}_model.h5'
+    file_name = f'files_{filename}/saved_models_{filename}/acc_{chosen_model}_model.keras'
 
     if train_model:
         input_shape = (X_train.shape[1], X_train.shape[2])
@@ -227,38 +233,22 @@ def train_sequential_model(X_train, y_train, X_test, y_test, chosen_model, class
     print("Train Accuracy: %d%%, Train Loss: %d%%" % (100 * accuracy, 100 * loss))
 
     probabilities = model.predict(X_test)
+    print(probabilities)
     window_size = 3
     threshold = 0.7
     y_test_labels = np.argmax(y_test, axis=1)
     y_pred_labels = np.argmax(probabilities, axis=1)
-    smoothed_probs = np.zeros_like(probabilities)
-
-    for i in range(len(probabilities)):
-        start = max(0, i - window_size + 1)
-        end = i + 1
-        smoothed_probs[i] = np.mean(probabilities[start:end], axis=0)
+    print(y_pred_labels)
 
     smoothed_predictions = []
-    for i, probs in enumerate(smoothed_probs):
-        max_prob = np.max(probs)
-        if max_prob >= threshold:
-            smoothed_predictions.append(np.argmax(probs))
-        else:
-            # If below threshold, retain previous prediction or mark as uncertain (-1)
-            smoothed_predictions.append(smoothed_predictions[-1] if smoothed_predictions else 1)
+
 
     # Calculate accuracy and other metrics
     print("Accuracy with initial predictions: ", round(100 * accuracy_score(y_test_labels, y_pred_labels), 2))
     print("F1 score with initial predictions :",
           round(100 * f1_score(y_test_labels, y_pred_labels, average='weighted'), 2))
-    print("Accuracy with smoothed predictions: ", round(100 * accuracy_score(y_test_labels, smoothed_predictions), 2))
-    print("F1 score with smoothed predictions: ",
-          round(100 * f1_score(y_test_labels, smoothed_predictions, average='weighted'), 2))
-
     print("\nClassification Report for initial predictions: :")
     print(classification_report(y_test_labels, y_pred_labels, target_names=class_labels))
-    print("\nClassification Report for smoothed predictions: :")
-    print(classification_report(y_test_labels, smoothed_predictions, target_names=class_labels))
 
     activity_predictions_true = np.empty(len(y_test_labels), dtype=object)
     for i in range(0, len(y_test_labels)):
@@ -267,10 +257,6 @@ def train_sequential_model(X_train, y_train, X_test, y_test, chosen_model, class
     activity_predictions = np.empty(len(y_pred_labels), dtype=object)
     for i in range(0, len(y_pred_labels)):
         activity_predictions[i] = class_labels[y_pred_labels[i]]
-
-    activity_predictions_smoothed = np.empty(len(smoothed_predictions), dtype=object)
-    for i in range(0, len(smoothed_predictions)):
-        activity_predictions_smoothed[i] = class_labels[smoothed_predictions[i]]
 
     smoothed_predictions = np.array(smoothed_predictions)
     return y_test_labels, y_pred_labels, smoothed_predictions
@@ -311,30 +297,6 @@ def plot_confusion_matrix(y_test_labels, y_pred_labels, smoothed_predictions, cl
         plt.tight_layout()
         plt.savefig(f'{path}/{plot_name}', bbox_inches='tight', pad_inches=0.1)
 
-    # Smoothed Values
-    for norm_value in normalize_cm:
-        if norm_value == 'true':
-            format = '.2f'
-            plot_name = f'acc_{chosen_model}_cm_norm_smooth.png'
-        else:
-            format = 'd'
-            plot_name = f'acc_{chosen_model}_cm_smooth.png'
-
-        disp = ConfusionMatrixDisplay.from_predictions(
-            y_test_labels, smoothed_predictions,
-            display_labels=class_labels,
-            normalize=norm_value,
-            xticks_rotation=70,
-            values_format=format,
-            cmap=plt.cm.Blues
-        )
-
-        plt.figure(figsize=(8, 10))
-        plt.title(f'Confusion Matrix for {chosen_model}')
-        disp.plot(cmap=plt.cm.Blues, values_format=format)
-        plt.xticks(rotation=70)
-        plt.tight_layout()
-        plt.savefig(f'{path}/{plot_name}', bbox_inches='tight', pad_inches=0.1)
 
 
 def plot_confusion_matrix_grouped(y_test_labels, y_pred_labels, smoothed_predictions, class_labels, chosen_model, filename):
@@ -398,21 +360,6 @@ def plot_confusion_matrix_grouped(y_test_labels, y_pred_labels, smoothed_predict
         plt.savefig(f'{path}/{plot_name}', bbox_inches='tight', pad_inches=0.1)
 
 
-def merge_activity_periods(y_labels, class_labels):
-    """
-    This function takes the activity labels and combine activities with the same label to create larger periods of a certain activity.
-    """
-    grouped_labels = [y_labels[0]]
-    for label in y_labels[1:]:
-        if label != grouped_labels[-1]:
-            grouped_labels.append(label)
-
-    grouped_labels = np.array(grouped_labels)
-    grouped_class_labels = [class_labels[label] for label in grouped_labels]
-    # print(len(grouped_class_labels)
-    return grouped_class_labels
-
-
 def group_categories(y_labels, class_labels):
     """
     This function takes the activity labels and groups them to more generic categories, exercising, idle, walking, sleeping
@@ -443,11 +390,13 @@ if __name__ == '__main__':
     frequency = 25
     time_required_ms = 10000
     samples_required = int(time_required_ms * frequency / 1000)
-    class_labels = ['cycling', 'exercising', 'lying', 'running', 'sitting', 'sleeping', 'standing', 'walking']
+    # class_labels = ['cycling', 'exercising', 'lying', 'running', 'sitting', 'sleeping', 'standing', 'walking']
+    class_labels = ['running', 'sitting', 'standing', 'walking']
     category_labels = ['exercising', 'idle', 'sleeping', 'walking']
     train_path = "../process_datasets/train_data_9.csv"
     test_path = "../process_datasets/test_data_9.csv"
-    filename = f"{time_required_ms}ms_8_classes"
+    my_test_path = "../process_datasets/final_my_data_collector.csv"
+    filename = f"{time_required_ms}ms_4_classes_mine"
 
     print(f'\nTraining 8 classes from file: {train_path}')
     print('Timesteps per timeseries: ', time_required_ms)
@@ -457,7 +406,8 @@ if __name__ == '__main__':
     models = ['cnn_cnn_lstm']
     # models = ['cnn_lstm','cnn_gru', 'cnn_cnn_lstm', 'cnn_cnn_gru']
     X_train, y_train, unique_activities = process_data(train_path, samples_required, False)
-    X_test, y_test, _ = process_data(test_path, samples_required, True)
+    # X_test, y_test, _ = process_data(test_path, samples_required, True)
+    X_test, y_test, _ = process_data(my_test_path, samples_required, True)
 
     # display_data(train_path, filename, False)
     # display_data(test_path, filename, True)
@@ -471,11 +421,6 @@ if __name__ == '__main__':
                                                                                     class_labels, filename,
                                                                                     train_model=False)
         plot_confusion_matrix(y_test_labels, y_pred_labels, smoothed_predictions, class_labels, chosen_model, filename)
-
-        # Merge activity periods
-        # grouped_class_y_labels = merge_activity_periods(y_test_labels, class_labels)
-        # grouped_class_labels = merge_activity_periods(y_pred_labels, class_labels)
-        # grouped_class_labels_smooth = merge_activity_periods(smoothed_predictions, class_labels)
 
         # Make predictions with generic categories
         predicted_y_categories = group_categories(y_test_labels, class_labels)
